485d2a6b07642c989837b993650e7f7ef55338b0
ProY9 <64046861+ProY9@users.noreply.github.com>
2020-04-21 00:37:38 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+var fun = 5;
+
+function funFunction() {
+    var fun = 'helooooo';
+    console.log(fun);
+}
+
+
+
+
+
+
+
+function calculate(num1, operation, num2) {
+    if(operation === "+") {
+       return num1 + num2;
+       } else if (operation === "-") {
+        return num1 - num2;
+    } else if (operation != "+" || operation != "-"){
+               return  `what the fuck`;
+               }
+};
+
+
+
+
+
+
+
+
+
+
+//var li0 = document.getElementsByTagName("li")[0];
+//var li1 = document.getElementsByTagName("li")[1];
+//
+//li0.addEventListener("click", function() {
+//    document.getElementsByTagName("li")[0].className = "coolTitle";
+//})
+//
+//li1.addEventListener("click", function() {
+//    document.getElementsByTagName("li")[1].className = "coolTitle";
+//})
